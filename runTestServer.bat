7eda920919af38bdff2a3de027d6a972cd454fb6
Gustavo Lopes Queiroz <gustavo.lopes.queiroz@gmail.com>
2020-07-27 23:03:56 -0600

    First simplest base of a game

---
+++ runTestServer.bat
@@ -0,0 +1,1 @@
+call python -m http.server
---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    //game.load.setBaseURL('http://labs.phaser.io');
+
+    this.load.image('sky', '../assets/bear.png');
+    this.load.image('logo', '../assets/crocodile.png');
+    this.load.image('red', '../assets/elephant.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    var particles = this.add.particles('red');
+
+    var emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    var logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+}
